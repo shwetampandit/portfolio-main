efbd4d24f5e03228ea1abcab6ca6789c15adebec
Shweta Pandit <shwetampandit@gmail.com>
2025-06-05 01:50:11 +0530

    Updated exports

--- src/components/navbar/index.ts
+++ src/components/navbar/index.ts
@@ -1,2 +1,4 @@
-export * from "./animated-text";
-export * from "./moving-element";
+// export * from "./animated-text";
+// export * from "./moving-element";
+export {  AnimatedText } from './animated-text';
+export {  MovingElement } from './moving-element';
--- src/components/sections/index.ts
+++ src/components/sections/index.ts
@@ -1,8 +1,16 @@
-export * from "./about-me";
-export * from "./education";
-export * from "./experience";
-export * from "./footer";
-export * from "./header";
-export * from "./navbar";
-export * from "./projects";
-export * from "./skills";
+// export * from "./about-me";
+// export * from "./education";
+// export * from "./experience";
+// export * from "./footer";
+// export * from "./header";
+// export * from "./navbar";
+// export * from "./projects";
+// export * from "./skills";
+export {AboutMe} from "./about-me"
+export {Education} from "./education";
+export {Experience} from "./experience";
+export {Footer} from "./footer";
+export {Header} from "./header";
+export {Navbar} from "./navbar";
+export {Projects} from "./projects";
+export {Skills} from "./skills";
